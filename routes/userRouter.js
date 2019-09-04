5fb25106718a6f8ef17fe4de9ba99cc9aa0353a0
copeju8 <51244110+copeju8@users.noreply.github.com>
2019-09-04 14:15:47 -0400

    updates route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,7 @@
 // Configures the Sequelize ORM
 const express = require('express');
 const router = express.Router();
+const User = require('../models').User;
 const { check, validationResult } = require('express-validator/check');
 
 router.get('/', (req, res) => {
@@ -9,8 +10,7 @@ router.get('/', (req, res) => {
 });
 });
 
-
-router.get('/users', (req, res, next) => {
+router.get('/users', (req, res) => {
  User.findAll()
    .then(users => {
      res.json(users)
@@ -18,38 +18,36 @@ router.get('/users', (req, res, next) => {
  
 //POST /api/users route (also defined in the routes.js file) creates a new user account:
 
-router.post('/users', (req, res) => {
-  // Get the user from the request body.
-  const user = req.body;
+// router.post('/users', (req, res) => {
+//   // Get the user from the request body.
+//   const user = req.body;
 
-  const errors = [];
+//   const errors = [];
 
-  // Validate that we have a `name` value.
-  if (!user.name) {
-    errors.push('Please provide a value for "name"');
-  }
+//   // Validate that we have a `name` value.
+//   if (!user.name) {
+//     errors.push('Please provide a value for "name"');
+//   }
 
-  // Validate that we have an `email` value.
-  if (!user.email) {
-    errors.push('Please provide a value for "email"');
-  }
+//   // Validate that we have an `email` value.
+//   if (!user.email) {
+//     errors.push('Please provide a value for "email"');
+//   }
 
-  // // If there are any errors...
-  // if (errors.length > 0) {
-  //   // Return the validation errors to the client.
-  //   res.status(400).json({ errors });
-  // } else {
+//   // // If there are any errors...
+//   // if (errors.length > 0) {
+//   //   // Return the validation errors to the client.
+//   //   res.status(400).json({ errors });
+//   // } else {
 
-  // Add the user to the `users` array.
-  users.push(user);
+//   // Add the user to the `users` array.
+//   users.push(user);
 
-  // Set the status to 201 Created and end the response.
-  res.status(201).end();
-  });
+//   // Set the status to 201 Created and end the response.
+//   res.status(201).end();
+//   });
 
-
+});
   
-
-
 
 module.exports = router;
