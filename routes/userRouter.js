0f1ce02f48ecdaf90f9b0b28e62af83f9ae2579b
copeju8 <51244110+copeju8@users.noreply.github.com>
2019-09-05 13:16:11 -0400

    Updates user Id

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -14,16 +14,70 @@ const auth = require('basic-auth');
 // const auth = require('basic-auth');
 // const bcryptjs = require('bcrypt');
 
-router.get('/', (req, res, next) => {
-  // try{
-  //   const student = await User.findAll()
-  //     res.json(student)
-  // } catch (err) {
-  //   next(err)
-  // }
-  res.json(userData);
-})
+//User authentication middleware function
+const authenticateUser = async (req, res, next) => {
+  let message;
+  // Parse the user's credentials from the Authorization header.
+  const credentials = auth(req);
+  if(credentials) {
+    //Find user with matching email address
+    const user = await User.findOne({
+        raw: true,
+        where: {
+          emailAddress: credentials.name,
+        },
+    });
+    //If user matches email
+    if(user) {
+      // Use the bcryptjs npm package to compare the user's password
+      // (from the Authorization header) to the user's password
+      // that was retrieved from the data store.
+      const authenticated = bcryptjs.compareSync(credentials.pass, user.password);
+      //If password matches
+      if(authenticated) {
+        console.log(`Authentication successful for user: ${user.firstName} ${user.lastName}`);
+        if(req.originalUrl.includes('courses')) {
+          //If route has a courses endpoint, set request userId to matched user id
+          req.body.userId = user.id;
+        } else if(req.originalUrl.includes('users')) {
+          //If route has a users endpoint, set request id to matched user id
+          req.body.id = user.id;
+        }
+      } else {
+        //Otherwise the Authentication failed
+        message = `Authentication failed for user: ${user.firstName} ${user.lastName}`;
+      }
+    } else {
+      // No email matching the Authorization header
+      message = `User not found for email address: ${credentials.name}`;
+    }
+  } else {
+    //No user credentials/authorization header available
+    message = 'Authorization header not found';
+  }
+  // Deny Access if there is anything stored in message
+  if(message) {
+    console.warn(message);
+    const err = new Error('Access Denied');
+    err.status = 401;
+    next(err);
+  } else {
+    //User authenticated
+    next();
+  }
+}
 
+router.get('/', authenticateUser, async (req, res) => {
+    const user = await User.findByPk(
+     req.body.id,
+  { 
+    attributes: {
+      exclude: ['password', 'createdAt', 'updatedAt'],
+    },
+  }
+); 
+  res.json(user);
+})
 
 //User's Routes
 
@@ -38,6 +92,7 @@ router.get('/', (req, res, next) => {
 // This array is used to keep track of user records as they are created.
 
 // Route that creates a new user.
+
 router.post('/', (req, res) => {
   //If there is a password
   if(req.body.password) {
